26fc4b746fdd1a1271c8bfb514dc31ef84c76103
Jofra Montesdeoca <jmontesdeocanuez21@gmail.com>
2024-06-05 10:29:14 +0100

    feat: display fee payments from mainchain

--- types/api/txsFilters.ts
+++ types/api/txsFilters.ts
@@ -4,6 +4,10 @@ export type TTxsFilters = {
   method?: Array<MethodFilter>;
 }
 
+export interface FeePaymentsFilters {
+  value_from_mainchain: 'true';
+}
+
 export type TypeFilter = 'token_transfer' | 'contract_creation' | 'contract_call' | 'coin_transfer' | 'token_creation';
 
 export type MethodFilter = 'approve' | 'transfer' | 'multicall' | 'mint' | 'commit';
