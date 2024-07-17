088080fc4205e91d0c1c8a75e587d66be6168169
Jofra Montesdeoca <jmontesdeocanuez21@gmail.com>
2024-07-17 16:52:14 +0100

    fix: ci broke

--- ci/setup_docker.sh
+++ ci/setup_docker.sh
@@ -1,7 +1,7 @@
 # install docker and docker-compose (compose v2 now included as plugin with apt install)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo systemctl stop containerd.service
